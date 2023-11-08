2b7b9501e7ad4dc6ca06dfc653a94618333cd8d7
Jan <jan.tecklenburg@gmx.de>
2023-11-08 14:25:27 +0100

    Added some error handling and option to pass boundary directly.

--- voromesh/voronoi/__init__.py
+++ voromesh/voronoi/__init__.py
@@ -8,23 +8,26 @@ Created on Thu Apr 21 10:49:20 2022
 import matplotlib.pyplot as plt
 from .helper import convex_hull, voronoi_bound
 from .io import to_vtk, to_pyvista
-from numpy import array
+from numpy import array, shape
 from scipy.spatial import Delaunay
+from shapely.geometry.polygon import Polygon
 
 
 class Voronoi:
-    def __init__(self, points, buffer_size):
+    def __init__(self, points, buffer_size=1, boundary=None):
         """
         Bounded 2D Voronoi decomposition for given points.
-        The bound is calculated from the buffered convex hull
-        of given points with the given buffer_size.
+        The boundary is either calculated from the buffered convex hull of the
+        given points with the given buffer_size (default) or passed directly.
 
         Parameters
         ----------
         points : numpy.array of shape (npoints, ndim=2)
             Coordinates of points to construct a Voronoi diagram from.
-        buffer_size : float
-            Buffer for convex hull > 0.
+        buffer_size : float, Optional
+            Buffer for convex hull >= 0.
+        boundary : shapely.geometry.polygon.Polygon, Optional
+            Boundary for Voronoi decomposition.
 
         Returns
         -------
@@ -32,8 +35,25 @@ class Voronoi:
 
         """
 
-        self.buffer_size = buffer_size
-        boundary = convex_hull(points, buffer_size)
+        n = shape(points)
+
+        if not n[1] == 2:
+            raise TypeError("Points needs to be an array of shape Nx2, but is "
+                            + str(n[0]) + "x" + str(n[1]) + "!")
+
+        if buffer_size < 0:
+            raise ValueError("Buffer_size needs to be >= 0!")
+
+        if boundary is None:
+            self.buffer_size = buffer_size
+            boundary = convex_hull(points, buffer_size)
+        elif type(boundary) is Polygon:
+            self.buffer_size = None
+        else:
+            self.buffer_size = None
+            raise Warning("Boundary is not of type"
+                          + "shapely.geometry.polygon.Polygon.\n"
+                          + "This can lead to unexpected results!")
 
         self.voronoi, self.points = voronoi_bound(points, boundary)
 
